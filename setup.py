09d82e065f53565fb7fe836d1dff62e3229af6b8
Riedler <david.riedler@gelse.net>
2019-10-21 12:25:15 +0200

    added setup.py for compiling into binary

---
+++ setup.py
@@ -0,0 +1,8 @@
+from cx_Freeze import setup, Executable
+build_exe_options = {}
+
+setup(name = "Rtris",
+      version = "1.0",
+      description = "",
+      options = { "build_exe" : build_exe_options },
+      executables = [Executable("rtris.py")])
